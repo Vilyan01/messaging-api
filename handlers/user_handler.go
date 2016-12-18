e9a50bf9ddcb2d9a3932f751565880a0f4f13485
Brian Heller <bheller2010@live.com>
2016-12-18 15:14:34 -0800

    Update user handler and main

--- handlers/user_handler.go
+++ handlers/user_handler.go
@@ -1,10 +1,25 @@
 package handlers
 
 import (
-	"errors"
+	//"errors"
+	"github.com/Vilyan01/messaging-api/data"
 	"net/http"
 )
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello"))
+	// find the user
+	user, err := data.FindUserById(1)
+	// check for error
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	// render the user in JSON form
+	user_json, err := json.Marshal(user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(user_json)
 }
--- main.go
+++ main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"github.com/Vilyan01/messaging-api/handlers"
 	"github.com/gorilla/mux"
+	"log"
+	"net/http"
 )
 
 func main() {
 	// create new router
 	r := mux.NewRouter()
+
+	// define routes
 	r.HandleFunc("/users/{id}", handlers.GetUser)
+
+	// start server
+	log.Fatal(http.ListenAndServe(":3000", r))
 }
