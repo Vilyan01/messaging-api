fd2a653c6899f819afa1bdad88ee4bdd2d7dc47c
Brian Heller <bheller2010@live.com>
2016-12-18 13:57:34 -0800

    Clean up main.go

--- main.go
+++ main.go
@@ -6,23 +6,13 @@ import (
 	"github.com/martini-contrib/render"
 )
 
-type Test struct {
-	Name string `json:"name"`
-}
-
 func main() {
 	// Use a classic martini server to host the API
 	m := martini.Classic()
 	// Use the renderer to easily render JSON/HTML
 	m.Use(render.Renderer())
 	// Test function for now...
-	m.Get("/", RenderTest)
 	m.Get("/users/:id", handlers.GetUser)
 	// Start the API
 	m.Run()
-}
-
-func RenderTest(r render.Render) {
-	n := Test{Name: "Heya"}
-	r.JSON(200, n)
 }
